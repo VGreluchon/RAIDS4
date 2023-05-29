c1d3724b8d6471eeb28140d1f68f0d3fad59150f
VGreluchon <tatanemercier@hotmail.fr>
2023-05-29 15:14:29 +0200

    reduction bordure

--- raid_uhc/data/raid_uhc/functions/game/in-game/bordertime.py
+++ raid_uhc/data/raid_uhc/functions/game/in-game/bordertime.py
@@ -1,23 +1,11 @@
-max = 5400
-min = 0
+max = 2500
+min = 300
 f = open("temps.txt", "w")
 while min <= max:
-	heure = (min//3600)
-	minute = (min//60) % 60
-	second = min % 60
-	if heure >= 10:
-		heure = str(heure)
-	else:
-		heure = '0'+str(heure)
-	if minute >= 10:
-		minute = str(minute)
-	else:
-		minute = '0'+str(minute)
-	if second >= 10:
-		second = str(second)
-	else:
-		second = '0'+str(second)
-
-	f.write('execute if score #temps raid.data matches '+str(min)+' run team modify raid.sidebar.6 suffix [{"text":"rdure","color":"aqua"},{"text":" » ","color":"gray"},{"text":"'+heure+':'+minute+':'+second+'","color":"green"}]\n')
-	min += 1
+	taille = min
+	taille = str(taille)
+	taille2 = min+4
+	taille2 = str(taille2)
+	f.write('execute if score #temps raid.data matches '+str(min)+'..'+str(taille2)+' run team modify raid.sidebar.5 suffix [{"text":"ille","color":"aqua"},{"text":" » ","color":"gray"},{"text":"'+taille+' blocs","color":"green"}]\n')
+	min += 5
 f.close()
